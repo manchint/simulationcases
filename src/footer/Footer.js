4d7f479bd09a69b2b168d28854ea573831cbcf17
manchint <152920263+manchite@users.noreply.github.com>
2025-04-30 19:34:24 -0400

    footer clean

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,7 +6,6 @@ const Footer = ({}) => {
     return (
         <div className="footer-container">
             <p>© 2025 Sri Rakumari Mendu | Pulse Scenarios</p>
-            {/* <p>We’d love to hear from you—share your thoughts, questions, or feedback in the comments below.</p> */}
         </div>     
     )
 
